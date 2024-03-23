0804825e859c38165bbb3e01289ab68838a92b3d
LuigiMottola <sputnick427@gmail.com>
2024-03-23 12:03:35 +0100

    termine esercizio

---
+++ src/main/java/com/example/springrepositories1/Entities/Car.java
@@ -0,0 +1,58 @@
+package com.example.springrepositories1.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String modelName;
+    @Column(nullable = false)
+    private String serialNumber;
+    @Column(nullable = false)
+    private Double currentPrice;
+
+    public Car() {
+    }
+
+    public Car(Long id, String modelName, String serialNumber, Double currentPrice) {
+        this.id = id;
+        this.modelName = modelName;
+        this.serialNumber = serialNumber;
+        this.currentPrice = currentPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+}
